496231a651cd5554f4f1ae840f18b2499f6db2d3
Eric Wyne <ecwyne@gmail.com>
2014-09-30 10:51:46 -0700

    server only and bump version

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
   summary: "infusionsoft SDK packaged for Meteor",
-  version: "1.0.0",
+  version: "1.0.1",
   git: 'http://github.com/ecwyne/meteor-infusionsoft',
   name: 'ecwyne:infusionsoft'
 });
 
 Npm.depends({
-	infusionsoft: '0.3.2'
+	infusionsoft: '0.3.3'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.2.2');
-  api.addFiles('infusionsoft.js');
+  api.addFiles('infusionsoft.js', 'server');
 });
